37cef95820b9fd7e1cd5eacba2ebb5b116339cae
Marcello <marcello.99.mp@gmail.com>
2025-12-19 20:34:45 -0500

    Test Unitarios

--- src/main/java/com/example/demo/service/RiskRemoteClient.java
+++ src/main/java/com/example/demo/service/RiskRemoteClient.java
@@ -1,16 +1,16 @@
 package com.example.demo.service;
 
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
-
-import java.math.BigDecimal;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
 
 @Service
 @RequiredArgsConstructor
@@ -19,33 +19,31 @@ public class RiskRemoteClient {
   private final WebClient riskWebClient;
   private final RiskService legacyRisk;
 
-  public Mono<Boolean> isAllowed(String currency, String type, BigDecimal amount) {
-    return Mono.fromCompletionStage(isAllowedTimed(currency, type, amount));
-  }
-
-  @TimeLimiter(name = "riskClient", fallbackMethod = "fallbackTimed")
+  @TimeLimiter(name = "riskClient")
   @Retry(name = "riskClient")
-  @CircuitBreaker(name = "riskClient", fallbackMethod = "fallbackTimed")
-  public CompletionStage<Boolean> isAllowedTimed(String currency, String type, BigDecimal amount) {
-    return callRemote(currency, type, amount)
-        .toFuture(); 
-  }
-
-  private Mono<Boolean> callRemote(String currency, String type, BigDecimal amount) {
+  @CircuitBreaker(name = "riskClient", fallbackMethod = "fallback")
+  public Mono<Boolean> isAllowed(String currency, String type, BigDecimal amount) {
     return riskWebClient.get()
         .uri(uri -> uri.path("/allow")
             .queryParam("currency", currency)
             .queryParam("type", type)
             .queryParam("amount", amount)
             .queryParam("fail", false)
-            .queryParam("delayMs", 200) 
+            .queryParam("delayMs", 200) // simula latencia
             .build())
         .retrieve()
         .bodyToMono(Boolean.class);
   }
 
-  public CompletionStage<Boolean> fallbackTimed(String currency, String type, BigDecimal amount, Throwable ex) {
-    return legacyRisk.isAllowed(currency, type, amount)
-        .toFuture();
+  public Mono<Boolean> fallback(String currency, String type, BigDecimal amount, Throwable ex) {
+    return legacyRisk.isAllowed(currency, type, amount);
+
+  }
+
+  @Autowired
+  RiskService legacy; // el de JPA que ya tenías
+
+  private Mono<Boolean> legacyAllowed(String c, String t, BigDecimal a) {
+    return legacy.isAllowed(c, t, a); // ya corre en boundedElastic
   }
 }
--- src/test/java/com/example/demo/service/RiskRemoteClientTest.java
+++ src/test/java/com/example/demo/service/RiskRemoteClientTest.java
@@ -2,6 +2,8 @@ package com.example.demo.service;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.web.reactive.function.client.WebClient;
+
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
@@ -20,8 +22,26 @@ class RiskRemoteClientTest {
         .thenReturn(Mono.just(false));
 
     StepVerifier.create(
-        Mono.fromCompletionStage(client.fallbackTimed("PEN", "DEBIT", new BigDecimal("1600"), new RuntimeException("down")))
-    ).expectNext(false)
-     .verifyComplete();
+        client.fallback("PEN", "DEBIT", new BigDecimal("1600"), new RuntimeException("down"))).expectNext(false)
+        .verifyComplete();
+  }
+
+  @Test
+  void isAllowed_should_return_true() {
+/*
+    WebClient webClient = WebClient.builder()
+        .baseUrl("http://localhost:8080")
+        .build();
+*/
+    var legacy = Mockito.mock(RiskService.class);
+    var client = new RiskRemoteClient(null, legacy);
+
+    when(legacy.isAllowed("001-0001", "DEBIT", new BigDecimal("100.00")))
+        .thenReturn(Mono.just(true));
+
+    StepVerifier.create(
+        client.isAllowed("PEN", "DEBIT", new BigDecimal("1600"))).expectNext(false)
+        .verifyComplete();
+
   }
 }
